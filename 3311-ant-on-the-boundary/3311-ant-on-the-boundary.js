f42a499be8998f84520b1d1a06a34e51c70732e8
Sabith Mk <115177227+Sabithx@users.noreply.github.com>
2024-09-03 19:22:07 +0530

    Time: 59 ms (52.63%), Space: 49.7 MB (61.4%) - LeetHub

---
+++ 3311-ant-on-the-boundary/3311-ant-on-the-boundary.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var returnToBoundaryCount = function (nums) {
+  f=0
+  nums.reduce((acc,e)=>{
+        t= acc+e
+        if(t==0){
+            f++
+        }
+       return  t
+    })
+    return f
+};
